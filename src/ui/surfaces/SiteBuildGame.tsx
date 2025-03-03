c4c6f7e3b228b6abf0a3f2d95810d37b9dcb8842
Ramon Snir <r@mon.dev>
2025-03-03 10:19:21 -0500

    dummy commit for success

--- src/ui/surfaces/SiteBuildGame.tsx
+++ src/ui/surfaces/SiteBuildGame.tsx
@@ -1,19 +1,16 @@
-import {
-  Card,
-  CardTitle,
-  SiteBuildGameSurface,
-} from "@netlify/sdk/ui/react/components";
 import { useNetlifySDK } from "@netlify/sdk/ui/react";
 
 export const SiteBuildGame = () => {
   const sdk = useNetlifySDK();
 
-  return (
-    <SiteBuildGameSurface>
-      <Card>
-        <CardTitle>Example Section for {sdk.extension.name}</CardTitle>
-        <p>This is an example site configuration.</p>
-      </Card>
-    </SiteBuildGameSurface>
-  );
+  return <></>;
+
+  // return (
+  //   <SiteBuildGameSurface>
+  //     <Card>
+  //       <CardTitle>Example Section for {sdk.extension.name}</CardTitle>
+  //       <p>This is an example site configuration.</p>
+  //     </Card>
+  //   </SiteBuildGameSurface>
+  // );
 };
